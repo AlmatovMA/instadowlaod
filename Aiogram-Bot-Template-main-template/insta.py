8a18c18d31ff07c21f23929ea5b5c4acf2fa89dd
unknown <instal@OGK-WN-1001.HG.loc>
2023-02-21 13:53:08 +0800

    bon on insta down

---
+++ Aiogram-Bot-Template-main-template/insta.py
@@ -0,0 +1,25 @@
+import requests
+import json
+def instadown(link):
+    url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
+
+    querystring = {"url":link}
+
+    headers = {
+        "X-RapidAPI-Key": "2cd5bfb8c7mshd9728daca531f13p118fdfjsn77c84124efd2",
+        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    rest = json.loads(response.text)
+    dict = {}
+    if rest['Type']=='Carousel':
+        dict['type']='carousel'
+        dict['media']=rest['media']
+        
+    
+
+    
+    
+    print(dict)
+instadown("https://www.instagram.com/p/Co6DhHvjkjh/?igshid=YmMyMTA2M2Y=")
---
+++ instawown/handlers/users/send.py
@@ -0,0 +1,21 @@
+from aiogram import types
+from loader import dp, Bot
+from aiogram.dispatcher.filters import Text
+from insta import instadown
+@dp.message_handler(Text(startswith="https://www.instagram.com/"))
+async def send_media(message:types.Message):
+    link = message.text
+    data = instadown(link=link)
+    if data == 'Bad':
+        await message.answer('bu URL manzil orkali hech narsa topa olmadik')
+    else:
+        if data['type']=='image':
+            await message.answer_photo(photo=data['media'])
+        elif data['type']=='video':
+            await message.answer_video(video=data['media'])
+        elif data['type']=='carousel':
+            for i in data['media']:
+
+             await message.answer_document(document=i)
+        else:
+            await message.answer('bu URL manzil orkali hech narsa topa olmadik')
